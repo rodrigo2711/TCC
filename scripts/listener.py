77d56382967da2f8a6b4b8e26b4f0c2b38b3af4b
Rodrigo2711 <rodrigo.sousa2711@gmail.com>
2019-05-24 22:36:35 -0300

    Adiciona listener e altera arquivos desnecessários da pasta scripts para src

--- scripts/listener.py
+++ scripts/listener.py
@@ -51,7 +51,7 @@ def listener():
     # run simultaneously.
     rospy.init_node('listener', anonymous=True)
 
-    rospy.Subscriber('chatter', String, callback)
+    rospy.Subscriber('/mpu6050', String, callback)
 
     # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
